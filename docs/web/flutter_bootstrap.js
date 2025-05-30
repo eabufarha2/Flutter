51b4ab70d90f1139fb1e87af9b7c5ecaf2ecc2b5
Elias Abufarha <54408382+eabufarha2@users.noreply.github.com>
2025-05-31 00:21:26 +0300

    Remove unused documentation assets and update service worker version

--- docs/web/flutter_bootstrap.js
+++ docs/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2253300677"
+    serviceWorkerVersion: "2733743528"
   }
 });
--- docs/web/flutter_service_worker.js
+++ docs/web/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "74696dd0811af89dc8cb0659fbf69cae",
+"flutter_bootstrap.js": "32f05d7570fcb4afda0e1cdc60b5c5e3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "fbaa0ee9a856cdabe7ab1b38dd30cdbb",
-"/": "fbaa0ee9a856cdabe7ab1b38dd30cdbb",
-"main.dart.js": "52933cb87d7bd5e45915a545c06c2bd8",
+"index.html": "d1aecc76bb8018fe3000f66248a24355",
+"/": "d1aecc76bb8018fe3000f66248a24355",
+"main.dart.js": "e5022347480ed846a7902deb76bb9187",
 "manifest.json": "1509278a18af5bb7a370125a59aaae7a",
 "version.json": "1fa92d599aaf58a93e1b6971eeea517e"};
 // The application shell files that are downloaded before a service worker can
